f85b577e86d18d9b06ce491501053bb53239b31b
jonas452 <jonasjordao452@gmail.com>
2017-01-16 12:46:26 -0300

    Runtime Annotation Problem
    Solved runtinme annotation problem using the @Retention:
    @Retention(RetentionPolicy.RUNTIME)

--- app/src/main/java/sape/cheetroid/lib/database/cfield/CPrimaryKey.java
+++ app/src/main/java/sape/cheetroid/lib/database/cfield/CPrimaryKey.java
@@ -1,5 +1,9 @@
 package sape.cheetroid.lib.database.cfield;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CPrimaryKey
 {
 
--- app/src/main/java/sape/cheetroid/lib/database/ctable/CTableAnno.java
+++ app/src/main/java/sape/cheetroid/lib/database/ctable/CTableAnno.java
@@ -1,5 +1,9 @@
 package sape.cheetroid.lib.database.ctable;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CTableAnno
 {
 
